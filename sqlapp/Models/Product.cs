5322cc4e8b2454c10029c3235715f0ba05cf71a8
Esteban-Valverde <esteban.valverde@xira.ai>
2022-06-28 01:13:22 -0500

    Agregar archivos de proyecto.

---
+++ sqlapp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace sqlapp.Models
+{
+    public class Product
+    {
+        public int ProductID { set; get; }
+
+        public string ProductName { set; get; }
+
+        public int Quantity { set; get; }
+    }
+}
